a3452e37b409a31cb46a5209277e3e7cdd50b2ef
jtomkiew <spratfear@gmail.com>
2016-04-14 21:04:27 +0200

    Added solution and basic tests.

---
+++ PseudoJsonToJson/JsonAmbulance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace PseudoJsonToJson
+{
+    public class JsonAmbulance
+    {
+        public string FixJsonString(string json)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PseudoJsonToJson/PseudoJson.Tests/JsonTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using PseudoJsonToJson;
+
+namespace PseudoJson.Tests
+{
+    [TestFixture]
+    public class JsonTests
+    {
+        public const string SmallGoodJson = "{ \"_id\": \"570fe32cebf5a9aca7e8ee54\","
+                                              + "\"index\": 0,"
+                                              + "\"guid\": \"a5558bd4-cad7-40cd-92d0-785d1f35bb15\","
+                                              + "\"isActive\": false,"
+                                              + "\"balance\": \"$1,324.92\","
+                                              + "\"picture\": \"http://placehold.it/32x32\","
+                                              + "\"age\": 30,"
+                                              + "\"eyeColor\": \"green\","
+                                              + "\"name\": \"Bridgette Miranda\", }";
+
+        public const string SmallBadJson = "{ \"_id\": \"570fe32cebf5a9aca7e8ee54\","
+                                           + "\"index\": 0,"
+                                           + "\"guid\": \"a5558bd4-cad7-40cd-92d0-785d1f35bb15\","
+                                           + "\"isActive\": false,"
+                                           + "\"balance\": $1,324.92,"
+                                           + "\"picture\": \"http://placehold.it/32x32\","
+                                           + "\"age\": 30,"
+                                           + "\"eyeColor\": green,"
+                                           + "\"name\": \"Bridgette Miranda\", }";
+
+        [Test]
+        public void CheckGoodSmallJsonString()
+        {
+            var ja = new JsonAmbulance();
+            var fixedJson = ja.FixJsonString(SmallGoodJson);
+            Assert.AreEqual(SmallGoodJson, fixedJson);
+        }
+
+        [Test]
+        public void FixBadSmallJsonString()
+        {
+            var ja = new JsonAmbulance();
+            var fixedJson = ja.FixJsonString(SmallBadJson);
+            Assert.AreNotEqual(SmallBadJson, fixedJson);
+            Assert.AreEqual(SmallGoodJson, fixedJson);
+        }
+    }
+}
