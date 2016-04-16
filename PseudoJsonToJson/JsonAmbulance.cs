31a979fe9a0a8d27449710a7f22822327ca1ef19
jtomkiew <spratfear@gmail.com>
2016-04-16 21:46:03 +0200

    Merge this!

--- PseudoJsonToJson/JsonAmbulance.cs
+++ PseudoJsonToJson/JsonAmbulance.cs
@@ -1,17 +1,188 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace PseudoJsonToJson
 {
     public class JsonAmbulance
     {
-        public string FixJsonString(string json)
+        public string FixSimpleJson(string json)
         {
+            if (IsValidJson(json))
+                return json;
+            GetNameValuePair(json.Trim());
             throw new NotImplementedException();
+
+            // Inny pomysł - czytanie wiadomości wyjątku z JObject.Parse() i rekursywne poprawienie całego json'a...
+        }
+
+        public static bool IsValidJson(string s)
+        {
+            try
+            {
+                JObject.Parse(s);
+            }
+            catch (JsonReaderException)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private void GetNameValuePair(string s)
+        {
+            var pattern = "\"[^\"]+?\"[^\"]*?:";
+            var regex = new Regex(pattern);
+            var matches = regex.Matches(s);
+            var indexesToFix = new Collection<int>();
+
+            foreach (Match match in matches)
+            {
+                var startIndex = match.Index + match.Length;
+                int valueLength;
+                var nextMatch = match.NextMatch();
+                if (nextMatch.Success)
+                    valueLength = nextMatch.Index - startIndex;
+                else
+                    valueLength = s.Length - startIndex - 1; // -1, as we want to ignore the last '}'
+                var value = s.Substring(startIndex, valueLength);
+                if (IsValidJson("{" + match.Value + value + "}"))
+                    continue;
+                var valueBorders = GetValueBorders(value);
+                indexesToFix.Add(startIndex + valueBorders[0]);
+                indexesToFix.Add(startIndex + valueBorders[1]);
+            }
+
+        }
+
+        private static int[] GetValueBorders(string value)
+        {
+            // Case of: |"value"|
+            if (value.StartsWith("\"") && value.EndsWith("\""))
+                return new[] {0, value.Length - 1};
+
+            // Case of: | "value", |
+            var firstQuotationMarkIndex = value.IndexOf("\"", StringComparison.InvariantCulture);
+            if (firstQuotationMarkIndex >= 0)
+            {
+                var lastQuotationMarkIndex = value.LastIndexOf("\"", StringComparison.InvariantCulture);
+                return new[] {firstQuotationMarkIndex, lastQuotationMarkIndex};
+            }
+
+            // Case of: | value, |
+            // Case of: | ,  |
+            var lastColonIndex = value.LastIndexOf(",", StringComparison.InvariantCulture);
+            var firstNonWhiteSpaceIndex = value.IndexOf(c => !char.IsWhiteSpace(c));
+            var lastNonWhiteSpaceIndex = value.LastIndexOf(c => !char.IsWhiteSpace(c));
+            if (lastColonIndex >= 0)
+            {
+                // Case of: | value1, value2  |
+                if (lastColonIndex == lastNonWhiteSpaceIndex)
+                {
+                    return new[] {firstNonWhiteSpaceIndex, lastColonIndex - 1};
+                }
+            }
+
+            // Case of: | value  |
+            if (lastNonWhiteSpaceIndex >= 0)
+            {
+                return new[] {firstNonWhiteSpaceIndex, lastNonWhiteSpaceIndex};
+            }
+
+            // Case of: |        |
+            return new[] {0, 0};
+        }
+
+        private static string ValueCleanUp(string value)
+        {
+            // Case of: |"value"|
+            if (value.StartsWith("\"") && value.EndsWith("\""))
+                return value;
+            int length;
+            // Case of: | "value", |
+            var firstQuotationMarkIndex = value.IndexOf("\"", StringComparison.InvariantCulture);
+            if (firstQuotationMarkIndex >= 0)
+            {
+                var lastQuotationMarkIndex = value.LastIndexOf("\"", StringComparison.InvariantCulture);
+                length = lastQuotationMarkIndex + 1 - firstQuotationMarkIndex;
+                return value.Substring(firstQuotationMarkIndex, length);
+            }
+
+            // Case of: | value, |
+            // Case of: | ,  |
+            var lastColonIndex = value.LastIndexOf(",", StringComparison.InvariantCulture);
+            var firstNonWhiteSpaceIndex = value.IndexOf(c => !char.IsWhiteSpace(c));
+            var lastNonWhiteSpaceIndex = value.LastIndexOf(c => !char.IsWhiteSpace(c));
+            if (lastColonIndex >= 0)
+            {
+                // Case of: | value1, value2  |
+                length = lastColonIndex - firstNonWhiteSpaceIndex;
+                if (lastColonIndex == lastNonWhiteSpaceIndex)
+                {
+                    return value.Substring(firstNonWhiteSpaceIndex, length);
+                }
+            }
+
+            // Case of: | value  |
+            if (lastNonWhiteSpaceIndex >= 0)
+            {
+                length = lastNonWhiteSpaceIndex + 1 - firstNonWhiteSpaceIndex;
+                return value.Substring(firstNonWhiteSpaceIndex, length);
+            }
+
+            // Case of: |        |
+            return "";
+        }
+
+        private struct NameValuePair
+        {
+            public readonly string Name;
+            public readonly string Value;
+
+            public NameValuePair(string name, string value)
+            {
+                Name = name;
+                Value = value;
+            }
+        }
+    }
+
+    public static class StringExtention
+    {
+        public static int IndexOf<TSource>(this IEnumerable<TSource> source,
+            Func<TSource, bool> predicate)
+        {
+            var i = 0;
+
+            foreach (var element in source)
+            {
+                if (predicate(element))
+                    return i;
+
+                i++;
+            }
+
+            return -1;
+        }
+
+        public static int LastIndexOf<TSource>(this IEnumerable<TSource> source,
+            Func<TSource, bool> predicate)
+        {
+            var i = source.Count() - 1;
+
+            foreach (var element in source.Reverse())
+            {
+                if (predicate(element))
+                    return i;
+
+                i--;
+            }
+
+            return -1;
         }
     }
 }
--- PseudoJsonToJson/PseudoJson.Tests/JsonTests.cs
+++ PseudoJsonToJson/PseudoJson.Tests/JsonTests.cs
@@ -7,14 +7,14 @@ namespace PseudoJson.Tests
     public class JsonTests
     {
         public const string SmallGoodJson = "{ \"_id\": \"570fe32cebf5a9aca7e8ee54\","
-                                              + "\"index\": 0,"
-                                              + "\"guid\": \"a5558bd4-cad7-40cd-92d0-785d1f35bb15\","
-                                              + "\"isActive\": false,"
-                                              + "\"balance\": \"$1,324.92\","
-                                              + "\"picture\": \"http://placehold.it/32x32\","
-                                              + "\"age\": 30,"
-                                              + "\"eyeColor\": \"green\","
-                                              + "\"name\": \"Bridgette Miranda\", }";
+                                            + "\"index\": 0,"
+                                            + "\"guid\": \"a5558bd4-cad7-40cd-92d0-785d1f35bb15\","
+                                            + "\"isActive\": false,"
+                                            + "\"balance\": \"$1,324.92\","
+                                            + "\"picture\": \"http://placehold.it/32x32\","
+                                            + "\"age\": 30,"
+                                            + "\"eyeColor\": \"green\","
+                                            + "\"name\": \"Bridgette Miranda\" }";
 
         public const string SmallBadJson = "{ \"_id\": \"570fe32cebf5a9aca7e8ee54\","
                                            + "\"index\": 0,"
@@ -24,13 +24,13 @@ namespace PseudoJson.Tests
                                            + "\"picture\": \"http://placehold.it/32x32\","
                                            + "\"age\": 30,"
                                            + "\"eyeColor\": green,"
-                                           + "\"name\": \"Bridgette Miranda\", }";
+                                           + "\"name\": \"Bridgette Miranda\" }";
 
         [Test]
         public void CheckGoodSmallJsonString()
         {
             var ja = new JsonAmbulance();
-            var fixedJson = ja.FixJsonString(SmallGoodJson);
+            var fixedJson = ja.FixSimpleJson(SmallGoodJson);
             Assert.AreEqual(SmallGoodJson, fixedJson);
         }
 
@@ -38,9 +38,59 @@ namespace PseudoJson.Tests
         public void FixBadSmallJsonString()
         {
             var ja = new JsonAmbulance();
-            var fixedJson = ja.FixJsonString(SmallBadJson);
+            var fixedJson = ja.FixSimpleJson(SmallBadJson);
             Assert.AreNotEqual(SmallBadJson, fixedJson);
             Assert.AreEqual(SmallGoodJson, fixedJson);
+        }
+
+        [Test]
+        public void CheckJsons()
+        {
+            var okJson = "{\"name\":\"value\"}";
+            Assert.IsTrue(JsonAmbulance.IsValidJson(okJson));
+
+            var maybeJson = "{\"name\":\"va\"lue\"}";
+            Assert.IsFalse(JsonAmbulance.IsValidJson(maybeJson));
+
+            var colonJson = "{\"name\":\"value\",}";
+            Assert.IsTrue(JsonAmbulance.IsValidJson(colonJson));
+
+            var doubleColonJson = "{\"name\":\"value\",,}";
+            Assert.IsFalse(JsonAmbulance.IsValidJson(doubleColonJson));
+
+            var newLineJson = "{\"name\":\"value\"} \r\n";
+            Assert.IsTrue(JsonAmbulance.IsValidJson(newLineJson));
+        }
+
+        [Test]
+        public void TestTrimmer()
+        {
+            var message = "This is what I want";
+            var testString = "   \n " + message + "  \r\n ";
+            var trimmedString = testString.Trim();
+            Assert.AreEqual(message, trimmedString);
+        }
+
+        [Test]
+        public void IndexOfExtentionTests()
+        {
+            var whiteSpaces = " \r\n  ";
+            var testString = "asd fg hjkl";
+            var s = whiteSpaces + testString + whiteSpaces;
+            var index = whiteSpaces.Length;
+            Assert.AreEqual(index, s.IndexOf<char>(c => !char.IsWhiteSpace(c)));
+            Assert.AreEqual(testString[0], s[index]);
+        }
+
+        [Test]
+        public void LastIndexOfExtentionTests()
+        {
+            var whiteSpaces = " \r\n  ";
+            var testString = "asd fg hjkl";
+            var s = whiteSpaces + testString + whiteSpaces;
+            var index = whiteSpaces.Length + testString.Length - 1;
+            Assert.AreEqual(index, s.LastIndexOf<char>(c => !char.IsWhiteSpace(c)));
+            Assert.AreEqual(testString[testString.Length - 1], s[index]);
         }
     }
 }
